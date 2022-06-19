e48421df20f41fe43f8a20657a08d2274e993d25
Test Testov <RQDerz@yandex.ru>
2022-06-19 14:36:14 +0300

    Домашнее задание за 12-й урок

--- Android_1_03/app/src/main/java/com/example/android_1_03/items/DataNotesSource.java
+++ Android_1_03/app/src/main/java/com/example/android_1_03/items/DataNotesSource.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class DataNotesSource implements IDataNoteSource{
+public class DataNotesSource implements IDataNoteSource {
     private ArrayList<Note> list = new ArrayList<>();
 
     public DataNotesSource() {
@@ -14,13 +14,9 @@ public class DataNotesSource implements IDataNoteSource{
     }
 
     private void initList() {
-        for (int i = 0; i < 5; i++) {
-            int size = 1;
-            Date firstDate = new Date();
-            Note note = new Note(String.valueOf(i) + " note", String.valueOf(i) + " description",
-                    String.valueOf(firstDate), i);
-            list.add(note);
-        }
+        Note note = new Note("Example note", "Example description",
+                "", 0);
+        list.add(note);
     }
 
     @Override
